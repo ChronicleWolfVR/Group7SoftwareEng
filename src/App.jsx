380d200f89445fdb5eafc69364b9c00da57206af
ChronicleWolfVR <ravivarunpravin@gmail.com>
2025-01-23 11:27:04 +0000

    React Set up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import Header from "./components/Header/Header.jsx";
+// import CoreConcepts from "./components/CoreConcepts/CoreConcepts.jsx";
+// import TabButton from "./components/TabButton/TabButton.jsx";
+// import { CORE_CONCEPTS } from "./data-with-examples.js";
+// import { EXAMPLES } from "./data-with-examples.js";
+
+function App() {
+  return (
+    <>
+    <main>
+      <h2>Hello world</h2>
+    </main>
+    </>
+  );
+}
+
+export default App;
