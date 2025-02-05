fac11626ea2c14f78b88befd6c1b0c4c35fc4353
ChronicleWolfVR <ravivarunpravin@gmail.com>
2025-02-05 15:06:28 +0000

    Basic tab and content added

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../Title/Title';
-import Button from '../Button/Button';
+import TabButton from './TabButton/TabButton';
 import './HomePage.css';
 
 const HomePage = () => {
+  const [selectedTab, setSelectedTab] = useState('Overview');
+
+  const tabs = ['Overview', 'Lights', 'Thermostat', 'Smart Plugs', 'Robots'];
+
   return (
+    <>
+    <button className="menu-button">=</button>
     <div className="home-page">
-      <button className="menu-button">=</button>
-      <div id="centered-container">
-      <Title id="titlep2">Smart Home</Title>
+        <Title id="titlep2">Smart Home</Title>
+        <menu id="examples">
+          {tabs.map((tab) => (
+            <TabButton
+              key={tab}
+              isSelected={selectedTab === tab}
+              onClick={() => setSelectedTab(tab)}
+            >
+              {tab}
+            </TabButton>
+          ))}
+        </menu>
+        <div id="tab-content">
+          {selectedTab === 'Overview' && <div>Overview Content</div>}
+          {selectedTab === 'Lights' && <div>Lights Content</div>}
+          {selectedTab === 'Thermostat' && <div>Thermostat Content</div>}
+          {selectedTab === 'Smart Plugs' && <div>Smart Plugs Content</div>}
+          {selectedTab === 'Robots' && <div>Robots Content</div>}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
