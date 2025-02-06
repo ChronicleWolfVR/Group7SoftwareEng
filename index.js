8f845721a9568df5aa48775107873eed1f937aa1
ChronicleWolfVR <ravivarunpravin@gmail.com>
2025-02-06 21:55:04 +0000

    trying to get the server to work

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the React app
+// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Handle any requests that don't match the ones above
+// Handle all routes by serving the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
