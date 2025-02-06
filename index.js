dce080f115fcaa39746998610243720a0ec0d369
ChronicleWolfVR <ravivarunpravin@gmail.com>
2025-02-06 21:23:32 +0000

    added heroku deployment files

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../FrontEnd/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../FrontEnd/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
